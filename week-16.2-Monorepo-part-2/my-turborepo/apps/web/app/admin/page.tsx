26f8de803a0bf0787d36d7a2d0e40b14db7f4898
Soumik Ghosh <realgoblu@gmail.com>
2024-04-07 06:47:52 +0530

    updated

---
+++ week-16.2-Monorepo-part-2/my-turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo.ui/Admin";
+import { InputBox } from "@repo/ui/input-box";
+export default function(){
+    return <div>
+        <Admin/>
+        <InputBox/>
+    </div>
+}
