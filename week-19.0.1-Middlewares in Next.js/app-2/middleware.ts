88ea715444622d2813087673b6b2a5b6323d34e3
Soumik Ghosh <realgoblu@gmail.com>
2024-04-26 16:11:19 +0530

    updated

---
+++ week-19.0.1-Middlewares in Next.js/app-2/middleware.ts
@@ -0,0 +1,14 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+let requestCount = 0;
+export function middleware(request: NextRequest) {
+  requestCount++;
+  console.log("number of requests is " + requestCount);
+  return  NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/api/:path*',//selective middleware
+}
