65bddcfe095bba5aa5fa5e0f8475712f0c28fb99
Soumik Ghosh <realgoblu@gmail.com>
2024-05-07 04:09:17 +0530

    updated

---
+++ week-20.2-OpenAPI-Spec/cohort-2-hono-openapi-spec/part-1/openAPIspec/index.ts
@@ -0,0 +1,31 @@
+const express = require('express');
+import swaggerUi from 'swagger-ui-express';
+import { openapispec } from './openapispec';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' }
+];
+
+app.get('/users', (req:any, res:any) => {
+    
+    const { name } = req.query;
+
+    if (name) {
+        const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
+        res.json(filteredUsers);
+    } else {
+        res.json(users);
+    }
+});
+
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(openapispec));
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
